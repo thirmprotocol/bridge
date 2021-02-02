7d0026c4a70633ea2d2241654e9cb56168a3efde
l3lackcurtains <info@crumet.com>
2021-02-02 00:12:03 -0700

    connect wallet page

--- src/container/WalletConnect/style.js
+++ src/container/WalletConnect/style.js
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 
 export const ConnectWrapper = styled.div`
-  margin: 120px 24px;
+  margin: 180px 8px;
   text-align: center;
   .heading-info {
     font-size: 45px;
+    margin: 24px 0;
   }
   .sub-heading-info {
     font-size: 18px;
-    margin: 24px 0;
+    margin: 36px 0;
   }
   .button-label {
     margin-left: 16px;
