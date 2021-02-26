1f6c8733107e2ac97eded82538c00c6cd88ea9a1
l3lackcurtains <l3lackcurtains@protonmail.com>
2021-02-25 21:28:56 -0700

    Stats link added

--- src/components/FooterBar/index.js
+++ src/components/FooterBar/index.js
@@ -10,13 +10,15 @@ function HeaderBar() {
         <li>
           <a href="//thirm.com" target="_blank" rel="noreferrer nofollow">Website</a>
         </li>
-
+        <li>
+          <a href="//stats.thirm.com" target="_blank" rel="noreferrer nofollow">Stats</a>
+        </li>
         <li>
           <a href="//docs.thirm.com" target="_blank" rel="noreferrer nofollow">Documentation</a>
         </li>
       </ul>
       <div>
-        <p>All Rights Reserved - 2021</p>
+        <p>All Rights Reserved. 2021</p>
       </div>
 
     </FooterWrapper>
